02f85b7b4c6bb480cb3cee6810c917fa5aaac6d1
Isaiah <isaiahbernal750@outlook.com>
2025-11-17 16:27:39 -0800

    Initialize script to run vulnerable Flask app

--- scripts/run_vulnerable_app.sh
+++ scripts/run_vulnerable_app.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
 
+source ../venv/bin/activate
+export FLASK_APP=vulnerable_app.py
+export FLASK_ENV=development
+export FLASK_DEBUG=1
+
+echo "Running Vulnerable App..."
+flask run --host=0.0.0.0 --port=5000
